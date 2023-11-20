4dbdbaf3dcb3800c2c2a401cdac18cd30ea297c0
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-20 23:00:18 +0600

    write js dection

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const draggable_list = document.getElementById('draggable-list');
-const check = document.getElementById('check');
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
 
-const richesrPeople = [
-  'Jeff Bezos',
-  'Bill Gates',
-  'Warren Buffet',
-  'Bernard Arnault',
-  'Carlos Slim Helu',
-  'Amancio Ortega',
-  'Larry ellison',
-  'Mark Zucarberg',
-  'Michel Bloomberg',
-  'Larry page'
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffet",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
 ];
 
-// Store listitems
-
+// Store list items
 const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+  richestPeople
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .forEach((person, index) => {
+      console.log(person);
+
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="person-name">${person.value}</p>
+      <i class="fa-solid fa-grip-lines"></i>
+    </div>
+    `;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+}
