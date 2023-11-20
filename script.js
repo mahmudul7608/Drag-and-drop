9f9e7fa608d6f33c9fe6ba26990d49fba595d8d3
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-20 22:09:45 +0600

    This is my drag and drop project

---
+++ script.js
@@ -0,0 +1,19 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richesrPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffet',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry ellison',
+  'Mark Zucarberg',
+  'Michel Bloomberg',
+  'Larry page'
+];
+
+// Store listitems
+
+const listItems = [];
